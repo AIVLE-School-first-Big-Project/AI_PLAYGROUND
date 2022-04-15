b542bd617cbc6621dc5f9eacf270e3340f299f0f
dhy02094 <dhy02074@naver.com>
2022-04-15 10:26:47 +0900

    ani

--- aiplay/urls.py
+++ aiplay/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('base/', include('base.urls')),
+    path('ani/', include('ani.urls'))
     
 ]
---
+++ ani/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from . import views
+
+from django.conf import settings 
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    # 어떤 글자가 오든지 views의 first_view 함수로 이동해라.
+    path('',views.first_view),
+    
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# 위 코드는 이미지 파일을 업로드 하기 위한 설정
+
+
