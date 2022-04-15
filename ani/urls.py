8af8a2292b478a333e1072e240fbd7759a5507f6
ehddbs1231 <ehddbs1231@naver.com>
2022-04-15 11:04:15 +0900

    ani test

--- ani/urls.py
+++ ani/urls.py
@@ -4,11 +4,11 @@ from . import views
 from django.conf import settings 
 from django.conf.urls.static import static
 
-
+app_name = 'ani'
 urlpatterns = [
     # 어떤 글자가 오든지 views의 first_view 함수로 이동해라.
     path('',views.first_view),
-    
+    path('write/', views.write, name = 'write'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # 위 코드는 이미지 파일을 업로드 하기 위한 설정
--- ani/views.py
+++ ani/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.utils import timezone
 
+from django.core.paginator import Paginator
+from django.db.models import Q
 # Create your views here.
 
 def first_view(request):
     return render(request, 'ani/first_view.html')
 
-
+def write(request):
+    return render(request, 'ani/write.html')
