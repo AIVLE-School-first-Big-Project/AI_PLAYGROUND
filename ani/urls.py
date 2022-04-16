5353e9f9a90aa4d4bba9c0929cf4bdbf4e59cb50
dhy02094 <dhy02074@naver.com>
2022-04-16 22:53:38 +0900

    헷갈려서 걍 커밋

--- ani/urls.py
+++ ani/urls.py
@@ -8,9 +8,9 @@ from django.conf.urls.static import static
 app_name = 'ani'
 urlpatterns = [
     # 어떤 글자가 오든지 views의 first_view 함수로 이동해라.
-    path('',views.first_view, name='first_view'),
+    #path('',views.first_view, name='first_view'),
     path('write/', views.write, name = 'write'),
-    path('fileupload/', views.fileUpload, name='fileupload'),
+    path('', views.fileUpload, name='fileupload'),
 ]
 
 # 위 코드는 이미지 파일을 업로드 하기 위한 설정
