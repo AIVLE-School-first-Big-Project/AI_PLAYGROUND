347ca36d707f9e1e30d1f582d9f1883cb59e8798
DHong68 <seek1225@gmail.com>
2022-04-24 22:48:35 +0900

    220424 flask-pcolor

---
+++ flask-pcolor/app.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask,redirect,url_for,request,render_template,jsonify
+import model,detect
+import json 
+app = Flask(__name__)
+@app.route('/colorpredict', methods=['POST','GET'])
+def predict():
+    if request.method=='POST':
+        f=request.files['file']
+        filename = f.filename
+        f.save(str(filename))
+        f_path = str(filename)
+        print(f_path)
+        result=detect.detect_face_masking(f_path)
+        result_dict={
+            'pcolor' : 'fail'
+        }
+        if result!='fail':
+            result=model.predict_pcolor(result)
+            result_dict = {
+            'pcolor' : str(result),
+        }
+        result_dict = json.dumps(result_dict)
+        
+
+    
+        return jsonify(result_dict)
+
+    
+        
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ flask-pcolor/detect.py
@@ -0,0 +1,39 @@
+import dlib
+import cv2
+import numpy as np
+import pandas as pd
+import os
+import requests
+import json
+face_detector = dlib.get_frontal_face_detector()
+lower = np.array([0,133,77], dtype = np.uint8)
+upper = np.array([255,173,127], dtype = np.uint8)
+def detect_face_masking(image_path):
+    img = cv2.imread(f'./{image_path}')
+    try:
+        faces = face_detector(img)
+    except:
+        os.remove(f'./{image_path}')
+        image_path='fail'
+    if len(faces) != 1 :
+        os.remove(f'./{image_path}')
+        image_path='fail'
+    else :
+        f = faces[0]
+        cropped_img = img[f.top()+2:f.bottom()-2, f.left()+2:f.right()-2]
+        scalePercent = 0.3
+        face_img_ycrcb = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2YCrCb)
+        skin_msk = cv2.inRange(face_img_ycrcb, lower, upper)
+        skin = cv2.bitwise_and(cropped_img, cropped_img, mask = skin_msk)
+        ratio_black = cv2.countNonZero(skin_msk)/(cropped_img.size/3)
+        colorPercent = (ratio_black * 100) / scalePercent
+        if colorPercent<=240:
+            os.remove(f'./{image_path}')
+            image_path='fail'
+            
+        else:
+            cv2.imwrite(f'./{image_path}',skin)
+    
+    return image_path
+
+        
