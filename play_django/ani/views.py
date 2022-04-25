79897a0e8c4d666c75e7a6b5abfc49e8fde337bf
dhy02094 <dhy02074@naver.com>
2022-04-25 12:22:55 +0900

    ani_bug1

--- play_django/ani/views.py
+++ play_django/ani/views.py
@@ -8,6 +8,7 @@ from django.db.models import Q
 import requests
 from .forms import FileUploadForm
 from .models import FileUpload
+import os
 # Create your views here.
 
 def first_view(request):
@@ -16,6 +17,32 @@ def first_view(request):
 def write(request):
     return render(request, 'ani/write.html')
 
+# def fileUpload(request):
+#     if request.method == 'POST':
+#         img = request.FILES['imgfile']
+#         fileupload = FileUpload(
+#             imgfile=img,
+#         )
+#         fileupload.save()
+#         print(fileupload.imgfile)
+        
+#         files = open("media/ani_images/"+img.name, 'rb')
+
+#         upload = {'file': files,
+#                 'filename':img.name
+#         }
+#         print(upload)
+#         res = requests.post('http://127.0.0.1:5000/predict', files = upload)
+#         print(res)
+#         fileupload.delete()
+#         return render(request, 'ani/result.html', upload)
+#     else:
+#         fileuploadForm = FileUploadForm
+#         context = {
+#             'fileuploadForm': fileuploadForm,
+#         }
+#         return render(request, 'ani/fileupload.html', context)
+
 def fileUpload(request):
     if request.method == 'POST':
         img = request.FILES['imgfile']
@@ -23,10 +50,12 @@ def fileUpload(request):
             imgfile=img,
         )
         fileupload.save()
+        print(fileupload.imgfile.name[2:5])
         print(img.name)
         files = open("media/ani_images/"+img.name, 'rb')
-        upload = {'file': files,
-                'filename':img.name
+
+        upload = {'file': fileupload.imgfile,
+                'filename':fileupload.imgfile.name[10:]
         }
         print(upload)
         res = requests.post('http://127.0.0.1:5000/predict', files = upload)
