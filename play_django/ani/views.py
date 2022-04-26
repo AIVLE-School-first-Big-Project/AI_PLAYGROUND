13714a2420222e81760b83af72175b47c213d302
dhy02094 <dhy02074@naver.com>
2022-04-26 10:43:11 +0900

    filename

--- play_django/ani/views.py
+++ play_django/ani/views.py
@@ -17,35 +17,10 @@ def first_view(request):
 def write(request):
     return render(request, 'ani/write.html')
 
-# def fileUpload(request):
-#     if request.method == 'POST':
-#         img = request.FILES['imgfile']
-#         fileupload = FileUpload(
-#             imgfile=img,
-#         )
-#         fileupload.save()
-#         print(fileupload.imgfile)
-        
-#         files = open("media/ani_images/"+img.name, 'rb')
-
-#         upload = {'file': files,
-#                 'filename':img.name
-#         }
-#         print(upload)
-#         res = requests.post('http://127.0.0.1:5000/predict', files = upload)
-#         print(res)
-#         fileupload.delete()
-#         return render(request, 'ani/result.html', upload)
-#     else:
-#         fileuploadForm = FileUploadForm
-#         context = {
-#             'fileuploadForm': fileuploadForm,
-#         }
-#         return render(request, 'ani/fileupload.html', context)
-
 def fileUpload(request):
     if request.method == 'POST':
         img = request.FILES['imgfile']
+        img.name = '1.jpg'
         fileupload = FileUpload(
             imgfile=img,
         )
