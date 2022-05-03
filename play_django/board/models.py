f0d1eb49d88a32a689f0ce5b06fd4aac811006e2
ehddbs1231 <ehddbs1231@naver.com>
2022-05-03 15:26:23 +0900

    board write 수정

--- play_django/board/models.py
+++ play_django/board/models.py
@@ -9,7 +9,7 @@ class Board(models.Model):
     title = models.CharField(max_length=50)
     model_name = models.CharField(max_length=50)
     body = models.TextField()
-    file = models.FileField(upload_to='', blank = True)
+    file = models.FileField(upload_to='board', blank = True)
 
     def __str__(self):
         return self.title
