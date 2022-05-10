4f8eb7639cb2be43ad81da856aef150ce96b46a4
JoonghyunChoi <wnd3735@gmail.com>
2022-05-10 09:52:44 +0900

    Update member

--- play_django/member/models.py
+++ play_django/member/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     id = models.AutoField(primary_key=True)
     user_id = models.CharField(max_length=20, unique=True, null=False)
     password = models.CharField(max_length=255, null=False)
-    profile_text = models.TextField(null=True)
+    email = models.CharField(max_length=50, unique=True, null=False)
     current_rftoken = models.CharField(max_length=255, null=True)
     createdAt = models.DateTimeField(auto_now_add=True)
 
